c7fdc1263a668dae260675c8ee4a10b1bb4df0fb
Jesus Vasquez <jesusvasquez384@gmail.com>
2023-01-29 18:19:36 -0500

    Mobile Styles

--- src/components/HeaderApp.jsx
+++ src/components/HeaderApp.jsx
@@ -5,12 +5,16 @@ const HeaderApp = () => {
     return ( 
         <header className='HeaderApp'>
             <div className='container'>
+                <div className='app-title'>
                 <h1 className='app-name text-3xl font-bold underline'>
                     Quick<span>Whatsapp</span>
                 </h1>
                 <h2 className='slogan'>
-                    Send messages to an unsaved numbers. 
+                    Chat with unsaved numbers. 
                 </h2>
+                </div>
+
+            
             </div>
         </header>
      );
