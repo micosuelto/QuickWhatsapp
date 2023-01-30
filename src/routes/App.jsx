282fc92693955f19b8765ca0915f00fe70304d4c
Jesus Vasquez <jesusvasquez384@gmail.com>
2023-01-30 16:21:21 -0500

    Config Manifest

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
+// Import Global Styles
+import "../styles/global.scss";
 // Import Pages
 import Home from "../pages/Home";
 import Credits from "../pages/Credits";
 import NotFound from "../pages/NotFound";
-// Import Global Styles
-import "../styles/global.scss";
+// Import Google Analytics
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-X3DY82NWV8";
+ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
   return (
